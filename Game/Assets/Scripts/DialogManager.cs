08b811bc90eccc7d461a48fd3949d7588dac9ca7
DOBBY0901 <ldjshfkd0901@gmail.com>
2025-12-01 21:43:56 +0900

    (2025.12.01) 지연보정

--- Game/Assets/Scripts/DialogManager.cs
+++ Game/Assets/Scripts/DialogManager.cs
@@ -1,51 +1,51 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using Photon.Pun;
 
-public class DialogManager : MonoBehaviour
+public class DialogManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] TMP_InputField inputField;
-    [SerializeField] Transform  content;
-    [SerializeField] GameObject message;
-    [SerializeField] ScrollRect scroll;
-
-    bool isChatting;
-
-    void Start()
-    {
-        inputField.onEndEdit.AddListener(InputChat);
-        isChatting = false;
-    }
+    [SerializeField] InputField inputField;
+    [SerializeField] Transform  parentTransform;
+    [SerializeField] ScrollRect scrollRect;
 
     private void Update()
     {
-        
-    }
-
-    void InputChat(string text)
-    {
-        if (!Input.GetKey(KeyCode.Return))
-            return;
-
-        Chat(text);
-
-        inputField.text = "";
-        
-    }
-
-    public void Chat(string text)
-    {
-        if (string.IsNullOrEmpty(text))
-            return;
-
-        GameObject messageobj = Instantiate(message, content);
-        Text messageText = messageobj.GetComponent<Text>();
-        messageText.text = text;
-
-        if(scroll != null)
+        if (Input.GetKeyDown(KeyCode.Return))
         {
+            inputField.ActivateInputField();
+
+            if (inputField.text.Length <= 0)
+            {
+                return;
+            }
+
+            string talk = inputField.text;
+            
+            //RPCTaeget.All 현재 룸의 모든 클라이언트에게 Talk함수 실행 명령
+
+            photonView.RPC("Send", RpcTarget.All, talk);
+
+            //텍스트 초기화
+            inputField.text = "";
+
+            //채팅 입력후에도 이어서 입력할수있도록
+            inputField.ActivateInputField();
+
+            //Canvas를 수동으로 동기화
             Canvas.ForceUpdateCanvases();
-            scroll.verticalNormalizedPosition = 0f;
+
+            //스크롤 위치를 초기화합니다.
+            scrollRect.verticalNormalizedPosition = 0f;
         }
+    }
+
+    [PunRPC]
+    public void Send(string message)
+    {
+        GameObject talk = Instantiate(Resources.Load<GameObject>("Talk"));
+        
+        talk.GetComponent<Text>().text = message;
+
+        talk.transform.SetParent(parentTransform, false);
     }
 }
