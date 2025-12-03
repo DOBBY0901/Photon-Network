965cce161811600e12a50a68d7c6811e322fa3d4
DOBBY0901 <ldjshfkd0901@gmail.com>
2025-12-03 21:43:25 +0900

    (2025.12.03) 추측 항법

--- Game/Assets/Scripts/RoomView.cs
+++ Game/Assets/Scripts/RoomView.cs
@@ -9,6 +9,16 @@ public class RoomView : MonoBehaviourPunCallbacks
 
     [SerializeField] string titletext;
 
+    [SerializeField] RoomInfo roominfo;
+
+    [SerializeField] Button button;
+
+    public event System.Action OnEntered;
+
+    private void Start()
+    {
+        OnEntered += UpdateRoomStatus;
+    }
     public void OnConnectRoom()
     {
         PhotonNetwork.JoinRoom(titletext);
@@ -20,9 +30,30 @@ public class RoomView : MonoBehaviourPunCallbacks
     }
     public void UpdateRoomInformation(RoomInfo roomInfo)
     {
+        this.roominfo = roomInfo;
+
         titletext = roomInfo.Name;
 
         roomtext.text = roomInfo.Name + " ( " + roomInfo.PlayerCount + " / " + roomInfo.MaxPlayers + " ) ";
-       
+
+        OnEntered?.Invoke();
+    }
+
+   public void UpdateRoomStatus()
+    {
+        if (roominfo.IsOpen)
+        {
+            button.interactable = true;
+        }
+        else
+        {
+            button.interactable = false;
+        }
+        
+    }
+
+    private void OnDestroy()
+    {
+            OnEntered -= UpdateRoomStatus;
     }
 }
